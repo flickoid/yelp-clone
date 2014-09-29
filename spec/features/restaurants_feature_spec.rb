b85b1f6d2c7e94a023477ddc934e4deddd31433c
flickoid <chris.oatley@gmail.com>
2014-09-29 17:24:08 +0100

    Added editing restaurants features

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -35,3 +35,17 @@ describe 'creating restaurants' do
   end
 end
 
+describe 'editing restaurants' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
+
+  it 'can allow a user to edit a restaurant' do
+    visit '/restaurants'
+    click_link 'Edit KFC'
+    fill_in 'Name', with: 'Kentucky Fried Chicken'
+    click_button 'Update Restaurant'
+    expect(page).to have_content 'Kentucky Fried Chicken'
+    expect(current_path).to eq '/restaurants'
+  end
+end
