bbab2bc46b884c521198551ec47b8f0622d6c3cf
flickoid <chris.oatley@gmail.com>
2014-09-29 17:47:33 +0100

    Added deleting restaurants functionality

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -49,3 +49,40 @@ describe 'editing restaurants' do
     expect(current_path).to eq '/restaurants'
   end
 end
+
+describe 'deleting restaurants' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
+
+  it 'removes the restaurant when user clicks delete link' do
+    visit '/restaurants'
+    click_link 'Delete KFC'
+    expect(page).not_to have_content 'KFC'
+    expect(page).to have_content "Restaurant successfully deleted"
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
