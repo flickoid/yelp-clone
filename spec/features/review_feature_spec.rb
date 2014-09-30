aebe5d8df4c35a5051bcae57d5aee9f69561c764
flickoid <chris.oatley@gmail.com>
2014-09-30 12:22:37 +0100

    Added functionality to add reviews and ratings to restaurants

---
+++ spec/features/review_feature_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'reviewing' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
+
+  it 'allows users to leave reviews using the form which appears alonside restaurants' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "average"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content "average"
+  end
+end
