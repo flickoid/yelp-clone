26811729ae925681833875b9fc82b287db302a98
flickoid <chris.oatley@gmail.com>
2014-09-30 19:05:22 +0100

    Added more tests for rating validation

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Review, :type => :model do
 
-  it "is not valid with a rating outside of the range 1-5" do
+  it "is valid with a rating between 1-5" do
+    review = Review.new(rating: 4)
+    expect(review).to be_valid
+  end
+
+  it "is not valid with a rating greater than 5" do
     review = Review.new(rating: 10)
+    expect(review).not_to be_valid
+  end
+
+  it "is not valid with a rating less than 1" do
+    review = Review.new(rating: -5)
     expect(review).not_to be_valid
   end
 end
