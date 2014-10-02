d04181ec22d5ca96ff80e05675d986331e54d6f7
flickoid <chris.oatley@gmail.com>
2014-10-02 11:28:32 +0100

    Added pluralize to like(s)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,9 @@
 class LikesController < ApplicationController
+  include ActionView::Helpers::TextHelper
 
   def create
     @review = Review.find(params[:review_id])
     @review.likes.create
-    render json: { new_like_count: @review.likes.count }
+    render json: { new_like_count: "#{pluralize(@review.likes.count, "like")}"}
   end
 end
