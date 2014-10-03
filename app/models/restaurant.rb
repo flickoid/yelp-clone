8df7fc5d114b2fc3297d22790984fbf644850337
flickoid <chris.oatley@gmail.com>
2014-10-03 15:45:08 +0100

    Added user authentication for restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "blank.jpeg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def average_rating
